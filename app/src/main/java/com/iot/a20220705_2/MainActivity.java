65856716b779a073494a2441bd3285f459d3cc27
ted4858 <ted4858@naver.com>
2022-07-05 17:03:12 +0900

    Initial commit

---
+++ app/src/main/java/com/iot/a20220705_2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.iot.a20220705_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
